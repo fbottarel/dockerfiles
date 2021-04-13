3c591f324de9f606e3204c426578f7f79212dd27
fbottarel <fabrizio.bottarel@iit.it>
2021-04-13 11:41:46 +0200

    added simple dockerfile to test nvidia+opengl capabilities

---
+++ test-opengl/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/#!/usr/bin/env bash
+
+# Expose the X server on the host.
+# This only works if the user is root though!
+# If we could create a runtime user, with the same UID/GID  we could get around this problem :)
+
+sudo xhost +local:root
+# --rm: Make the container ephemeral (delete on exit).
+# -it: Interactive TTY.
+# --gpus all: Expose all GPUs to the container.
+docker run \
+  --rm \
+  -it \
+  --gpus all \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  glvnd-x \
+  bash
+
+sudo xhost -local:root
